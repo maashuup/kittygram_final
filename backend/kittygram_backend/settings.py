d2712fccf0c2e952b9e2ed3fc94383c74ba119c0
Mariia Znobishcheva <lanvinmasha@gmail.com>
2025-02-17 17:19:00 +0100

    Correction of media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,7 +98,8 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/cats/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
